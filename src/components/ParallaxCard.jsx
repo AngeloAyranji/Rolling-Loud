ad0d921ce55e8d68c0bec91c72dba123dc52a3f2
RamySobhieh <ramysobhieh@email.com>
2023-05-31 17:54:14 +0200

    ADjusted video

--- src/components/ParallaxCard.jsx
+++ src/components/ParallaxCard.jsx
@@ -4,8 +4,8 @@ function ParallaxCard() {
   return (
     <div className="w-full flex justify-center px-8">
       <div className="w-full mx-auto flex items-center justify-center max-w-[1400px] mb-12">
-        <div className="flex flex-col items-center md:items-start md:flex-row rounded-lg bg-[#121212] border-2 border-primary w-full mb-10 drop-shadow-xl">
-          <div className="flex flex-col space-y-4 w-full lg:w-[60%] p-8">
+        <div className="flex flex-col items-center md:items-start md:flex-row rounded-lg bg-[#121212] border-2 border-primary w-full mb-10 relative">
+          <div className="flex flex-col space-y-4 w-full md:w-[60%] p-8 z-10 drop-shadow-xl">
             <h2 className="font-extrabold text-white text-3xl">
               FIRST-PERSON <span className="text-primary">VIEW</span>
             </h2>
@@ -22,12 +22,12 @@ function ParallaxCard() {
               <Link to="/products">EXPLORE</Link>
             </button>
           </div>
-          <div className="flex justify-center items-center w-[270px] md:w-[40%] h-[250px]">
-            <div className="relative w-full h-full">
-              <div className="absolute md:right-[-20px] md:top-[-20px] top-10">
-                <img src={drone} className=""></img>
-              </div>
-            </div>
+          <div className="flex justify-center items-center w-[95%] md:w-[500px] relative translate-y-8 md:absolute md:-right-4 md:top-4 md:translate-y-0 z-0">
+            <img
+              src={drone}
+              alt=""
+              className="opacity-100 md:opacity-50 xl:opacity-100"
+            />
           </div>
         </div>
       </div>
