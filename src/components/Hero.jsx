560d9ae0831ec4e2ee8b5a830b977e7c19f868ff
RamySobhieh <ramysobhieh@email.com>
2023-06-01 14:56:05 +0200

    Adjusted btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -89,7 +89,7 @@ function Hero() {
         </Carousel>
         <div className="absolute bottom-0 w-full h-[30%] bg-gradient-to-t from-[#121212] to-transparent z-[5]"></div>
         <motion.div
-          className="flex flex-row absolute top-[64%] sm:top-[60%] sm:left-[25%] left-[12%]"
+          className="flex flex-row absolute top-[64%] sm:top-[60%] sm:left-[25%] left-[12%] z-50"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
